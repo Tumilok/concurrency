e29a7bcf3e74aba5bfe131a88a38aa4d56a76a08
tumilok <vladislav.tumilovich@gmail.com>
2020-11-03 15:09:18 +0100

    Edit handler package

--- TW-lab4/src/main/java/pl/edu/agh/handler/thread/ConsumerHandler.java
+++ TW-lab4/src/main/java/pl/edu/agh/handler/thread/ConsumerHandler.java
@@ -1,18 +1,18 @@
 package pl.edu.agh.handler.thread;
 
-import pl.edu.agh.Storage;
+import pl.edu.agh.buffer.UnfairStorage;
 import pl.edu.agh.handler.portion.PortionHandler;
 import pl.edu.agh.thread.Consumer;
 
 public class ConsumerHandler extends WorkerHandler {
-    public ConsumerHandler(Storage storage, PortionHandler portionHandler, int workerNumber) {
-        super(storage, portionHandler, workerNumber);
+    public ConsumerHandler(UnfairStorage unfairStorage, PortionHandler portionHandler, int workerNumber) {
+        super(unfairStorage, portionHandler, workerNumber);
     }
 
     @Override
     public void createAndRunWorkers() {
         for (int i = 0; i < workerNumber; i++) {
-            workers.add(new Consumer(storage, portionHandler.getPortion()));
+            workers.add(new Consumer(unfairStorage, portionHandler.getPortion()));
             int finalI = i;
             new Thread(() -> workers.get(finalI).work()).start();
         }
