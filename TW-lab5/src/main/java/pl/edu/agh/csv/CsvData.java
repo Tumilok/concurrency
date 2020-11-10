83ac7a4959afcf51aa9bd57994b71cc45a7c875f
Uladzislau Tumilovich <vladislav.tumilovich@gmail.com>
2020-11-10 13:23:00 +0100

    Add csv handlers

--- TW-lab5/src/main/java/pl/edu/agh/csv/CsvData.java
+++ TW-lab5/src/main/java/pl/edu/agh/csv/CsvData.java
@@ -1,2 +1,19 @@
-package pl.edu.agh.csv;public class CsvData {
+package pl.edu.agh.csv;
+
+public class CsvData {
+
+    public static final String TITLES = "THREAD_NUM,TIME";
+
+    private final int threadNum;
+    private final long time;
+
+    public CsvData(int threadNum, long time) {
+        this.threadNum = threadNum;
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return threadNum + "," + time;
+    }
 }
--- TW-lab5/src/main/java/pl/edu/agh/csv/CsvFileWriter.java
+++ TW-lab5/src/main/java/pl/edu/agh/csv/CsvFileWriter.java
@@ -1,2 +1,41 @@
-package pl.edu.agh.csv;public class CsvFileWriter {
+package pl.edu.agh.csv;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class CsvFileWriter {
+    private static final String CSV_FILEPATH = "src/main/resources/result.csv";
+
+    private static final CsvFileWriter instance = new CsvFileWriter();
+
+    public static CsvFileWriter getInstance() {
+        return instance;
+    }
+
+    private final File csvOutputFile = new File(CSV_FILEPATH);
+
+    private CsvFileWriter() {
+        createNewCsvFile();
+    }
+
+    private void createNewCsvFile() {
+        try {
+            Files.deleteIfExists(csvOutputFile.toPath());
+            writeDataToCSV(CsvData.TITLES);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeDataToCSV(String line) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(csvOutputFile, true))) {
+            bw.append(line);
+            bw.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
